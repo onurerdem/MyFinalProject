1ff31a5fdd542e88c596275722e81a44781e8f1c
Onur Erdem <erdem.onur@outlook.com.tr>
2021-02-07 01:39:32 +0300

    6 Şubat ders sonu.

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            //İş kodları
+            //throw new NotImplementedException();
+            return _categoryDal.GetAll();
+        }
+
+        //Select * from Categories where CategoryId = 3
+        public Category GetById(int categoryId)
+        {
+            //throw new NotImplementedException();
+            return _categoryDal.Get(c => c.CategoryId == categoryId);
+        }
+    }
+}
