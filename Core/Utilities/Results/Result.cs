5b546d992cab17b8638ae17ac3aedd3e5dc1bdbb
Onur Erdem <erdem.onur@outlook.com.tr>
2021-02-11 00:31:42 +0300

    10 Şubat ders sonu.

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResults
+    {
+        //private bool v1;
+        //private string v2;
+
+        public Result(bool success, string message):this(success)
+        {
+            //this.v1 = v1;
+            //this.v2 = v2;
+            Message = message;
+            //Success = success;
+        }
+
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
