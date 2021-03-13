90eaf9c3979d8be0db249c9e59faa3b1f5739351
Onur Erdem <erdem.onur@outlook.com.tr>
2021-03-14 01:41:48 +0300

    13 Mart ders sonu.

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -39,7 +39,7 @@ namespace WebAPI.Controllers
             //Dependency chain --
             //IProductService productService = new ProductManager(new EfProductDal());
 
-            Thread.Sleep(5000);
+            Thread.Sleep(1000);
             
             var result = _productService.GetAll();
             if (result.Success)
@@ -65,6 +65,30 @@ namespace WebAPI.Controllers
         public IActionResult GetById(int id)
         {
             var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbycategory")]
+        public IActionResult GetByCategory(int categoryId)
+        {
+            var result = _productService.GetAllByCategoryId(categoryId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpGet("getproductdetails")]
+        public IActionResult GetProductDetails(int categoryId)
+        {
+            var result = _productService.GetProductDetails();
             if (result.Success)
             {
                 return Ok(result);
