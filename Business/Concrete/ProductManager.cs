67c062730c95cd46810643f164b4263227bea939
Onur Erdem <erdem.onur@outlook.com.tr>
2021-03-21 01:52:57 +0300

    20 Mart ders sonu.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -46,7 +46,7 @@ namespace Business.Concrete
         //[Validate]
         //00.25 Dersteyiz
         //Claim
-        [SecuredOperation("product.add,admin")]
+        //[SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
         [CacheRemoveAspect("IProductService.Get")]
         public IResults Add(Product product)
